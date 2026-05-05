e06a3f02a362c036f5d8877280a67728bb2acbc1
りnk4t <rockama6750emanuel@gmail.com>
2026-05-04 22:17:20 -0600

    wrong commit

--- engine/src/parser.rs
+++ engine/src/parser.rs
@@ -43,6 +43,8 @@ impl Diagnostic {
 
 } //I plan to change Diagnostic to its own rs only for simplicity( . .)φ
 
+//|-----------------{Lexer and Token( . .)φ}------------------|
+
 pub enum Token {
     Var(String),
     Ope(String),
@@ -61,4 +63,26 @@ impl Lexer {
         let ch_buffer: Vec<char> = entry.chars().collect();
         Lexer { expression: ch_buffer, pos: 0, diags: Diagnostic::new("Lexer".to_string()) }
     }
+
+    pub fn lex(&mut self) -> Token{
+        Token::Eof
+    }
+
+    pub fn next(&mut self) -> char{
+        let ch = self.peek();
+        self.pos += 1;
+        ch
+    }
+
+    pub fn peek(&mut self) -> char{
+        match self.expression.get(self.pos) {
+            Some(ch) => *ch,
+            None => '\0',
+        }
+    }
+
+    pub fn reset(&mut self){
+        self.pos = 0;
+    }
+
 }

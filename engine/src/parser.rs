069df28d1f1b72b916d696af6db49a62c69c41c0
りnk4t <rockama6750emanuel@gmail.com>
2026-05-04 21:59:31 -0600

    Diagnostic complete (⌒‿⌒)

--- engine/src/parser.rs
+++ engine/src/parser.rs
@@ -4,7 +4,6 @@
 // another struct and impl
 
 
-
 //|-----------------{Diagnostics ( . .)φ}------------------|
 
 pub struct Diagnostic{
@@ -16,4 +15,30 @@ impl Diagnostic{
     pub fn new(owner_str: String) -> Diagnostic{
         Diagnostic{msgs: Vec::new(), owner: owner_str}
     }
+
+    pub fn add_msg(&mut self, msg: &str){
+        let mut err_msg: String = self.owner.clone();
+        err_msg.push_str(format!(" {msg}").as_str());
+    }
+
+    fn add_err_msg(&mut self, msg: &str, token: Token){
+        let mut token_info: String = self.owner.clone();
+        token_info.push_str(match token {
+            Token::Var(data) => format!(": {msg} {data}"),
+            Token::Ope(data) => format!(": {msg} {data}"),
+            Token::Bad(data) => format!(": {msg} {data}"),
+            Token::Eof => format!(": Premature EOF"),
+        }.as_str());
+        
+        self.msgs.push(token_info);
+    }
+
+    pub fn is_empty(&self) -> bool{
+        self.msgs.is_empty()
+    }
+
+    pub fn get_msgs(&self) -> Vec<String>{
+        self.msgs.clone()
+    }
+
 }

cbd29551dcb350df409443ccfe7e5509ebba196b
りnk4t <rockama6750emanuel@gmail.com>
2026-05-05 20:50:03 -0600

    Properties struct added

---
+++ engine/src/properties.rs
@@ -0,0 +1,35 @@
+
+#[derive(Default)]
+pub struct Properties{
+    tautology: bool,
+    contradiction: bool,
+    satisfactory: bool,
+    contingent: bool,
+    equivalent: bool
+}
+
+impl Properties {
+    fn is_all_same<T: PartialEq>(&mut self, slice: &[T]) -> bool{
+        slice.first().map_or(true, |first| slice.iter().all(|x| x == first))
+    }
+
+    pub fn set_properties(&mut self, truth_vec: &Vec<bool>, equival: bool){
+        match truth_vec.first() {
+            Some(truval) => {
+                if self.is_all_same(truth_vec){
+                    if *truval{
+                        self.tautology = true;
+                        self.satisfactory = true;
+                    }else { self.contradiction = true }
+                }
+            },
+            None => {}
+        }
+
+        if !self.contradiction{ self.satisfactory =  true; }
+
+        if !self.tautology && !self.contradiction { self.contingent = true; }
+
+        self.equivalent = equival;
+    }
+}

ac3f999fb3c5e380c9fb8d8a067bde1094c8663d
りnk4t <rockama6750emanuel@gmail.com>
2026-05-05 19:04:17 -0600

    test added

--- engine/src/lib.rs
+++ engine/src/lib.rs
@@ -3,10 +3,38 @@ mod ast;
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use crate::{ast::ExpressionAST, parser::{Lexer, Parser}};
+
+use super::*;
 
     #[test]
     fn engine_test(){
+
+        let mut parser = Parser::new(Lexer::new("(a&b&)".to_string()));
+
+        if parser.plexer.diags.is_empty() {
+            if parser.diags.is_empty() {
+                let (asts, is) = parser.build_asts();
+
+                for x in asts {
+                    match x {
+                        ExpressionAST::Expression(id, tree) => {
+                            println!("expr id: {id}");
+                            tree.print_tree(" ", false, true);
+                        }
+                    }
+                }
+
+            }else {
+                for x in parser.diags.get_msgs() {
+                    println!("{x}");
+                }
+            }
+        }else {
+            for x in parser.plexer.diags.get_msgs() {
+                println!("{x}");
+            }
+        }
 
     }
     

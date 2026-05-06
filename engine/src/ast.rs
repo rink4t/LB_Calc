5e39b2a033304ead0749f2e7ad216bf30cf13a96
りnk4t <rockama6750emanuel@gmail.com>
2026-05-05 20:44:26 -0600

    ast evaluation

--- engine/src/ast.rs
+++ engine/src/ast.rs
@@ -1,4 +1,6 @@
 
+use std::collections::HashMap;
+
 use crate::parser::Token;
 
 //|-----------------{AST ( . .)φ}------------------|
@@ -27,5 +29,41 @@ impl AST {
         if let Some(lnode) = &self.left { lnode.print_tree(new_prefix.as_str(), true, false); }
 
         if let Some(rnode) = &self.right { rnode.print_tree(new_prefix.as_str(), false, false); }
+    }
+
+    fn value_from_variable(&self, var: &String, variables: &HashMap<String, Vec<bool>>, pos: usize) -> bool{
+        match variables.get(var).cloned() {
+            Some(value) => {
+                match value.get(pos) {
+                    Some(val) => *val,
+                    None => false,
+                }
+            },
+            None => false,
+        }
+    }
+
+    pub fn evaluate(&self, variables: &HashMap<String, Vec<bool>>, pos: usize) -> bool{
+
+        match &self.token {
+            Token::Var(x) => {
+                self.value_from_variable(x, &variables, pos)
+            },
+            
+            Token::Ope(op) => {
+                let l_eval: bool = self.left.as_ref().map(|left| left.evaluate(&variables, pos)).unwrap_or(false);
+                let r_eval: bool = self.right.as_ref().map(|right| right.evaluate(&variables, pos)).unwrap_or(false);
+
+                match op.as_str() {
+                    "∧" => l_eval && r_eval,
+                    "∨" => l_eval || r_eval,
+                    "¬" => !r_eval,
+                    "↔" => l_eval == r_eval,
+                    "→" => !l_eval || r_eval,
+                    _ => unreachable!()
+                }
+            }
+            _ => {unreachable!()}
+        }
     }
 }

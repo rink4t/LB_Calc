602f309cd3dbae052e94dd4f031e7b6ea66a1837
りnk4t <rockama6750emanuel@gmail.com>
2026-05-13 14:19:31 -0600

    tui prototype v2

--- src/tui.rs
+++ src/tui.rs
@@ -1,26 +1,44 @@
-use std::io;
+use std::io::{self, Stdout, stdout};
 
 use color_eyre::eyre::Ok;
-use crossterm::{ExecutableCommand, terminal::{self, EnterAlternateScreen, LeaveAlternateScreen, disable_raw_mode, enable_raw_mode}};
-use ratatui::{Terminal, backend::{self, CrosstermBackend}};
+use crossterm::{ExecutableCommand, terminal::{EnterAlternateScreen, LeaveAlternateScreen, disable_raw_mode, enable_raw_mode}};
+use ratatui::backend::{Backend, CrosstermBackend};
 
 
-pub fn init() -> color_eyre::Result<()> {
-    enable_raw_mode()?;
-    io::stdout().execute(EnterAlternateScreen)?;
-    Ok(())
+pub struct Tui {
+    terminal: ratatui::Terminal<CrosstermBackend<Stdout>>,
+    //event_hndl: 
 }
 
-pub fn shutdown() {
-    let leave_scr = io::stdout().execute(LeaveAlternateScreen).map(|_f| ());
+impl Tui {
 
-    if let Err(err) = leave_scr {
-        eprintln!("leave alternate screen failed (・・ヾ: \n{}", err);
+    pub fn new(tick_rate: u64) -> color_eyre::Result<Self>{
+        Ok(Self { 
+            terminal: ratatui::Terminal::new(CrosstermBackend::new(stdout()))?,
+        })
     }
 
-    let leave_raw_mode = disable_raw_mode();
+    pub fn init(&mut self) -> color_eyre::Result<()> {
+        enable_raw_mode()?;
+        io::stdout().execute(EnterAlternateScreen)?;
+        Ok(())
+    }
 
-    if let Err(err) = leave_raw_mode {
-        eprintln!("leave raw mode failed (・・ヾ: \n{}", err);
+    pub fn shutdown(&mut self) {
+        let leave_scr = io::stdout().execute(LeaveAlternateScreen).map(|_f| ());
+
+        if let Err(err) = leave_scr {
+            eprintln!("leave alternate screen failed (・・ヾ: \n{}", err);
+        }
+
+        let leave_raw_mode = disable_raw_mode();
+
+        if let Err(err) = leave_raw_mode {
+            eprintln!("leave raw mode failed (・・ヾ: \n{}", err);
+        }
+
+        if let Err(err) = self.terminal.show_cursor() {
+            eprintln!("show the cursor failed: (・・ヾ: \n{}", err);
+        }
     }
 }

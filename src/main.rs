d434ba04fe2ea973a54e631b445760cb8ee07c81
りnk4t <rockama6750emanuel@gmail.com>
2026-05-12 20:12:36 -0600

    prototype

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("First Stage and first commit");
+use std::io;
+use ratatui::{Terminal, backend::{CrosstermBackend}};
+
+// modules (/ =ω=)/
+mod tui;
+mod app;
+
+#[tokio::main]
+async fn main() -> color_eyre::Result<()> {
+
+    tui::init();
+
+    let backend = CrosstermBackend::new(io::stdout());
+    let mut terminal = Terminal::new(backend)?;
+
+    //let mut app = App::new(terminal);
+    //let res = app.run().await;
+
+    tui::shutdown();
+
+    Ok(())
 }

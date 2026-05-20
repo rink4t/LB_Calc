e971e235571acc416385825faad1718092867b77
りnk4t <rockama6750emanuel@gmail.com>
2026-05-19 19:44:59 -0600

    (　’ω’)旦~~

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -1,28 +1,28 @@
 use async_trait::async_trait;
 use ratatui::{Frame, layout::Rect};
-use crossterm::event::{KeyCode, KeyEvent};
+use crossterm::event::{KeyEvent};
 
 //modules (/ =ω=)/
 pub mod entry_comp;
 pub mod btn_comp;
-pub mod table;
+pub mod table_comp;
 
 //components use (≧◡≦)
 pub use entry_comp::EntryLineComp;
 pub use btn_comp::ButtonComp;
-pub use table::TableComp;
+pub use table_comp::TableComp;
 
 pub trait DrawableComp {
     fn draw(&self, f: &mut Frame, rect: Rect);
 }
 
 pub trait StatefullDrawableComp {
-    fn draw(&self, f: &mut Frame, rect: Rect) -> color_eyre::Result<()>;
+    fn draw(&mut self, f: &mut Frame, rect: Rect);
 }
 
 #[async_trait]
 pub trait Component {
-    fn event(&mut self, key: KeyEvent);
+    fn event(&mut self, key: KeyEvent) -> color_eyre::Result<()>;
 
     fn focus(&mut self, _focus: bool);
 }

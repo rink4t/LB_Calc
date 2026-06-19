b4e66f71e425757326f1b05ec946465f45359806
りnk4t <rockama6750emanuel@gmail.com>
2026-06-19 00:18:09 -0600

    Better table (≧◡≦)

--- src/components/table_comp.rs
+++ src/components/table_comp.rs
@@ -1,7 +1,7 @@
 use crossterm::event::{KeyCode, KeyEventKind};
-use ratatui::{layout::{Alignment, Constraint}, style::{Color, Style, Stylize}, text::Line, widgets::{Block, Borders, Cell, Padding, Paragraph, Row, Table, TableState}};
+use ratatui::{layout::{Alignment, Constraint}, style::{Color, Style, Stylize}, text::{Line}, widgets::{Block, Borders, Cell, Padding, Paragraph, Row, Table}};
 
-use crate::components::{Component, StatefullDrawableComp};
+use crate::components::{Component, DrawableComp};
 
 //|-----------------{Flags >ᴗ<}------------------|
 
@@ -14,29 +14,114 @@ enum Flag {
 //|-----------------{Table Comp( . .)φ}------------------|
 
 pub struct TableComp {
-    table_state: TableState, 
     ids: Vec<String>,
     colums: Vec<Vec<bool>>, 
     flag: Flag,
     focus: bool,
+    offset: usize,
+    height: usize,
+    selected: usize,
+    selected_colum: usize,
 }
 
 impl TableComp {
-    pub fn new(ids: Vec<String>, colums: Vec<Vec<bool>>, focus: bool) -> TableComp {
-        TableComp { table_state: TableState::default(), ids, colums, flag: Flag::ShowMsg, focus: focus }
+    pub fn _new(ids: Vec<String>, colums: Vec<Vec<bool>>, focus: bool) -> TableComp {
+        TableComp { ids, colums, flag: Flag::ShowMsg, focus: focus, offset: 0, height: 0, selected: 0, selected_colum: 0}
     }
 
     pub fn update(&mut self, ids: Vec<String>, colums: Vec<Vec<bool>>) {
         self.ids = ids;
         self.colums = colums;
+        self.offset = 0;
+        self.selected = 0;
         self.flag = Flag::ShowTable;
-        self.table_state.select(Some(0));
     }
 
     pub fn no_table(&mut self) {
         self.ids.clear();
         self.colums.clear();
         self.flag = Flag::ShowMsg;
+    }
+
+    fn next_row(&mut self) {
+
+        if self.flag == Flag::ShowMsg { return; }
+
+        let rows_t = self.colums[0].len();
+        let next_row = if self.selected + 1 < rows_t { self.selected + 1 } else { self.selected };
+    
+        self.selected = next_row;
+        
+        if next_row >= self.offset + self.height {
+            self.offset = (next_row.saturating_sub(self.height)) + 1;
+        }
+        //println!("{} h{}", self.height, height);
+        if self.offset + self.height > rows_t {
+            self.offset = rows_t.saturating_sub(self.height);       
+        }
+    }
+
+    fn prev_row(&mut self) {
+
+        if self.selected == 0 {return;}
+
+        if self.selected <= self.offset.saturating_sub(self.height) {
+            self.offset = self.offset.saturating_sub(1);
+        }
+
+        self.selected = self.selected.saturating_sub(1);
+
+        if self.selected < self.offset {
+            self.offset = self.selected;
+        }
+    }
+
+    fn next_colum(&mut self) {
+        if self.colums.is_empty() { return; }
+
+        if self.selected_colum >= self.ids.len().saturating_sub(1) {
+            return;
+        }
+
+        self.selected_colum += 1;
+
+    }
+
+    fn previous_column(&mut self) {
+        if self.colums.is_empty() { return; }
+
+        if self.selected_colum == 0 {
+            return;
+        }
+
+        self.selected_colum -= 1;
+    }
+
+    fn go_end(&mut self) {
+        if self.colums.is_empty() { return; }
+
+        let rows_t = self.colums[0].len();
+
+        self.selected = (rows_t).saturating_sub(1);
+        
+        if self.selected >= self.offset + self.height {
+            self.offset = (self.selected.saturating_sub(self.height)) + 1;
+        }
+  
+        if self.offset + self.height > rows_t {
+            self.offset = rows_t.saturating_sub(self.height);       
+        }
+    }
+
+    fn go_top(&mut self){
+        if self.colums.is_empty() || self.selected == 0{ return; }
+
+        self.selected = 0;
+
+        if self.selected < self.offset {
+            self.offset = self.selected;
+        }
+
     }
 
     pub fn get_keybinds(&self) -> String{
@@ -46,13 +131,17 @@ impl TableComp {
 
 impl Default for TableComp {
     fn default() -> Self {
-        TableComp { table_state: TableState::new(), ids: Vec::new(), colums: Vec::new(), flag: Flag::ShowMsg, focus: false }
+        TableComp { ids: Vec::new(), colums: Vec::new(), flag: Flag::ShowMsg, focus: false, offset: 0, height: 0, selected: 0, selected_colum: 0}
     }
 }
 
-impl StatefullDrawableComp for TableComp {
+impl DrawableComp for TableComp {
     fn draw(&mut self, f: &mut ratatui::prelude::Frame, rect: ratatui::prelude::Rect) {
         //Now cats guide us (=^-ω-^=)
+        
+        let height = (rect.height.saturating_sub(3)) as usize;
+        self.height = height;
+
         if self.flag == Flag::ShowTable {
             let header = Row::new(self.ids.iter().map(|item| Cell::from(Line::from(item.clone()).alignment(Alignment::Center)))).style(
                 if self.focus {
@@ -61,12 +150,33 @@ impl StatefullDrawableComp for TableComp {
                     Style::default()
                 }
             );    
-            let rows_n = self.colums[0].len();
 
-            let rows: Vec<Row> = (0..rows_n).map(|row_indx|{let cells: Vec<Line> = self.colums.iter()
-                .map(|col| if col[row_indx] {Line::from("T").fg(Color::Green).alignment(Alignment::Center)} else {Line::from("F").fg(Color::Red).alignment(Alignment::Center)})
-                .collect(); Row::new(cells)}).collect();
-            
+            let start = self.offset;
+            let rows_t = self.colums[0].len();
+            let end = (start + height).min(rows_t);
+
+            let rows: Vec<Row> = (start..end).map(|row_indx|{
+                let cells: Vec<Line> = self.colums
+                .iter().enumerate().map(|(col_indx,col)| {
+                    
+                    let val = if col[row_indx] {"T"} else {"F"};
+                    let color = if col[row_indx] { Color::Green } else { Color::Red };
+                    let mut line = Line::from(val).fg(color).alignment(Alignment::Center);
+
+                    if row_indx == self.selected && col_indx == self.selected_colum {
+                        line = line.bg(Color::Rgb(99, 9, 0))
+                    }
+
+                    line
+                }).collect(); 
+                let mut row = Row::new(cells);
+                if row_indx == self.selected {
+                    row = row.bg(Color::Rgb(36, 10, 7));
+                }
+                row
+                })
+            .collect();
+
             let widths: Vec<Constraint> = self.ids.iter().map(|cell| 
                 if cell.chars().count() >= 2 {
                     Constraint::Min(cell.chars().count() as u16)
@@ -76,10 +186,8 @@ impl StatefullDrawableComp for TableComp {
                 
             ).collect();
 
-            let table = Table::new(rows, widths).block(Block::new().borders(Borders::ALL)).header(header)
-                .cell_highlight_style(Style::new().bg(Color::Rgb(59, 8, 3))).row_highlight_style(Style::new().bg(Color::Rgb(36, 10, 7))).highlight_symbol(">>");
-
-            f.render_stateful_widget(table, rect, &mut self.table_state);
+            let table = Table::new(rows, widths).block(Block::new().borders(Borders::ALL)).header(header);
+            f.render_widget(table, rect);
 
         }else if self.flag == Flag::ShowMsg {
             let text = "(^０^)ノ Hiiii";
@@ -97,10 +205,12 @@ impl Component for TableComp {
     fn event(&mut self, key: crossterm::event::KeyEvent) -> color_eyre::Result<()> {
         if key.kind == KeyEventKind::Press {
             match key.code {
-                KeyCode::Up => {self.table_state.select_previous();},
-                KeyCode::Down => {self.table_state.select_next();},
-                KeyCode::Left => {self.table_state.select_previous_column();}
-                KeyCode::Right => {self.table_state.select_next_column();}
+                KeyCode::Up => { self.prev_row(); },
+                KeyCode::Down => { self.next_row(); },
+                KeyCode::Left => { self.previous_column(); }
+                KeyCode::Right => { self.next_colum(); }
+                KeyCode::End => { self.go_end(); }
+                KeyCode::Home => { self.go_top(); }
                 _ => {},
             }
         }
